4489e8207e0b455248dc7d20db6d7e7693ace172
kennexrazon <kennexrazon@gmail.com>
2017-06-06 10:20:55 +0800

    removed the dummy data in CMD6.ino changed some spacing.

--- DueLogger/CMD6.ino
+++ DueLogger/CMD6.ino
@@ -12,42 +12,39 @@ void getdataBroadcastNew(int mode,char readCode){
 
 
 		if (PRINT_MODE == 1) Serial.println("  --function: getdataBroadcastNew(), case T -- called");
+		
 		if (sensorVersion == 1){
-		Serial1.print("ARQWAIT");
-                GET_DATA(columnData,PASS_AXEL1_ADC_CALIB_MINMAX); 
-		Serial.println(columnData);
-		parsedData = parser(columnData,4);
-		writeData(parsedData);													//write data to sdcard
-		Serial.println(columnData);
-		Serial.println(parsedData);
-		sprintf(allData,parsedData ,strlen(parsedData));              
-                }else{
+			Serial1.print("ARQWAIT");
+	        GET_DATA(columnData,PASS_AXEL1_ADC_CALIB_MINMAX); 
+			Serial.println(columnData);
+			parsedData = parser(columnData,4);
+			writeData(parsedData);													//write data to sdcard
+			Serial.println(columnData);
+			Serial.println(parsedData);
+			sprintf(allData,parsedData ,strlen(parsedData));              
+        } else{
+			Serial1.print("ARQWAIT");        
+			GET_DATA(columnData,PASS_AXEL1_ADC_CALIB_MINMAX); 
+			String temp= "";
+			temp.toCharArray(columnData, 521);
+			Serial.println(columnData);
+			parsedData = parser(columnData,8);
+			// Serial.println("Right after 1ST parsedData = parser(columnData,8);");
+			writeData(parsedData);													//write data to sdcard
+			Serial.println(parsedData);
+			sprintf(allData,parsedData ,strlen(parsedData));		
 
-                Serial1.print("ARQWAIT");        
-		// GET_DATA(columnData,PASS_AXEL1_ADC_CALIB_MINMAX); 
-		String temp= "";
-		// temp="";
-		//temp= "000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914000F0200541703207914";
-		temp.toCharArray(columnData, 521);
-		Serial.println(columnData);
-		parsedData = parser(columnData,8);
-		Serial.println("Right after 1ST parsedData = parser(columnData,8);");
-		writeData(parsedData);													//write data to sdcard
-		Serial.println(parsedData);
-		sprintf(allData,parsedData ,strlen(parsedData));		
-
-		Serial1.print("ARQWAIT");                
-		// GET_DATA(columnData,PASS_AXEL2_ADC_CALIB_MINMAX);
-		temp="";
-		temp= "000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914000F0210541703207914";
-		temp.toCharArray(columnData, 521);
-		Serial.println(columnData);
-		parsedData = parser(columnData,8);
-		Serial.println("Right after 2ND parsedData = parser(columnData,8);");
-		writeData(parsedData);													//write data to sdcard
-		Serial.println(parsedData);
-		strncat(allData,"+" ,1);
-		strncat(allData,parsedData ,strlen(parsedData));}
+			Serial1.print("ARQWAIT");                
+			GET_DATA(columnData,PASS_AXEL2_ADC_CALIB_MINMAX);
+			temp.toCharArray(columnData, 521);
+			Serial.println(columnData);
+			parsedData = parser(columnData,8);
+			// Serial.println("Right after 2ND parsedData = parser(columnData,8);");
+			writeData(parsedData);													//write data to sdcard
+			Serial.println(parsedData);
+			strncat(allData,"+" ,1);
+			strncat(allData,parsedData ,strlen(parsedData));
+		}
                 
         if (PIEZO == 1) {		
     		Serial1.print("ARQWAIT");        
