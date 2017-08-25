b71fdb3ded562e4642911fc3e8fcaa680d67873e
eavmendoza <earl.mendoza@gmail.com>
2017-08-25 16:45:29 +0800

    Added inline arguments for health.py for read only routine

--- gateway/health.py
+++ gateway/health.py
@@ -5,8 +5,22 @@ import time, sys, memcache
 import powermon as pmon
 import gsmio
 import common
+import argparse
 
 def main():
+
+    parser = argparse.ArgumentParser(description = "Health routine [-options]")
+    parser.add_argument("-r", "--read_only", 
+        help = "do not save data to memory", action = 'store_true')
+
+    try:
+        args = parser.parse_args()
+    except IndexError:
+        print '>> Error in parsing arguments'
+        error = parser.format_help()
+        print error
+        sys.exit()
+
 
     ts = dt.today()
     ts_str = ts.strftime("%x,%X,")
@@ -24,7 +38,8 @@ def main():
     msgtosend += sysvol + ","
     msgtosend += csq
 
-    common.save_sms_to_memory(msgtosend)
+    if not args.read_only:
+        common.save_sms_to_memory(msgtosend)
     
 if __name__=='__main__':
     try:
