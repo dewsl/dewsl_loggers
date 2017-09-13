be50a0a64d74851f2ee5abce659027d29be56b5b
madmarkarnel <dmarkarnel@gmail.com>
2017-09-13 10:38:17 +0800

    relay switch added

--- extensometer/tx_arduino/tx_arduino.ino
+++ extensometer/tx_arduino/tx_arduino.ino
@@ -3,40 +3,43 @@
 #include <stdint.h>
 
 const byte LED = 13;
+const byte SWR = 12;
 const byte BUTTON = 2;
-volatile boolean flag;
+volatile boolean RELAY;
 volatile unsigned int count = 0;
+int y;
 
 uint8_t toggle4[]  = {0x00,0x20,0x10,0x20,0x10,0x10,0x20,0x10,0x20,0x00};
 
 void setup() {
-    
+    pinMode(SWR, OUTPUT);
     DDRD &= ~(1 << DDD2);   //Set pin 2 as input
     DDRD |= (1 << 4);       //Set pin 4 as output 
     DDRD |= (1 << 5);       //Set pin 5 as output  
-    PORTB |= 0b00100000;
-    PORTB &= ~0x20;       // set pin low
+    PORTB |= 0b00110000;    //set pin 12 and 13 as output
+    PORTB &= ~0x30;       // set pin 12 and 13 LOW
         
    TCCR1B = 0;                     // set up timer with prescaler = 1 and CTC mode
-   //    TCCR1B |= (1 << WGM12)|(1 << CS10);   //11
+   //TCCR1B |= (1 << WGM12)|(1 << CS10);   //11
    TCNT1 = 0;                    // initialize counter
    OCR1A = 0;                    // initialize compare value
    OCR1A = 175;       //200
    TIMSK1 |= (1 << OCIE1A);     //set interrupt bit
-   attachInterrupt(digitalPinToInterrupt (BUTTON), PING, FALLING);
+   attachInterrupt(digitalPinToInterrupt(BUTTON), PING, FALLING);
    sei();    
 }
 
 uint32_t x = 0;
 
 void PING(){
-
   x=0;
   while(x<1000){
     x++;
   }
+
   if (digitalRead(BUTTON) == 0x00) {
-      PORTB ^= 0x20;
+    RELAY = true;    // relay pin
+    PORTB ^= 0x20;
     TCCR1B |= (1 << WGM12)|(1 << CS10);
     }
 //  for(x=0; x<10000; x++){
@@ -59,5 +62,17 @@ ISR (TIMER1_COMPA_vect){
 }
 
 void loop() {
-
+  if(RELAY){
+    digitalWrite(SWR, HIGH);
+    delay(5000);
+    digitalWrite(SWR, LOW);
+    delay(100);
+    // PORTB |= 0X10;    //set relay pin high
+    // y=0;
+    // while(y<5000){
+    //   y++;
+    // }
+    // PORTB &= ~0X10;    //set relay pin low
+    RELAY = false;
+  }
 }
